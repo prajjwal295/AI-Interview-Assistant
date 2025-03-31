8b6859caf52d9b60b4286bdd5e1c0d480ffae7e6
prajjwal <mishraprajjwal295@gmail.com>
2025-04-01 01:06:41 +0530

    concept for the contest

---
+++ server/models/Contest.js
@@ -0,0 +1,12 @@
+// const MockInterview = require("./MockInterview");
+
+// const cONTEST = CREATE{
+
+//     CONTtest_id,
+//     topic,
+//     exper,
+//     questions
+//     timer:
+//     enrollerment : [users];
+//     interviers : [MockInterviewid]
+// }
